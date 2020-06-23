e7d7b46cd2f6036bee9c38811f1ca0fe17737d26
p1981thompson <paul.thompson@psy.ox.ac.uk>
2020-06-23 13:26:06 +0100

    something new

--- test.R
+++ test.R
@@ -1,21 +1,21 @@
-x <- runif(300,  min=-10, max=10) 
-y <- 0.3*x^3 - 0.25 * x^2 - x + 10 + rnorm(length(x),0,8) 
- 
+x <- runif(300,  min=-10, max=10)
+y <- 0.35*x^3 - 0.45 * x^2 - x + 10 + rnorm(length(x),0,8)
+
 # plot of x and y :
-plot(x,y,col=rgb(0.2,0.2,0.8,0.6),pch=16 , cex=1.3) 
- 
+plot(x,y,col=rgb(0.2,0.2,0.8,0.6),pch=16 , cex=1.3)
+
 # Can we find a polynome that fit this function ?
 model <- lm(y ~ x + I(x^2) + I(x^3))
- 
+
 # I can get the features of this model :
 #summary(model)
 #model$coefficients
 #summary(model)$adj.r.squared
- 
+
 # For each value of x, I can get the value of y estimated by the model, and add it to the current plot !
-myPredict <- predict( model ) 
+myPredict <- predict( model )
 ix <- sort(x,index.return=T)$ix
-lines(x[ix], myPredict[ix], col=2, lwd=2 )  
+lines(x[ix], myPredict[ix], col=2, lwd=2 )
 
 # I add the features of the model to the plot
 coeff <- round(model$coefficients , 2)
